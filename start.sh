21dd8221a56a19dd2b6804de7dc90fedc39bc4af
Pixel Perfect <pixelperfectpon3@gmail.com>
2017-07-17 01:16:12 -0400

    go is still crashing? add some debug stuff

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+echo Checking if log file is in Docker volume
 if [ ! -f /opt/config/citmp-server.yml ]; then
 	cp /opt/cfx-server/citmp-server.yml /opt/config
 fi
+echo Removing orignal config file
 rm /opt/cfx-server/citmp-server.yml
+echo Copying config file from Docker volume
 cp /opt/config/citmp-server.yml /opt/cfx-server/citmp-server.yml
+echo Moving into server directory
 cd /opt/cfx-server
+echo Running run.sh
 sh /opt/cfx-server/run.sh
