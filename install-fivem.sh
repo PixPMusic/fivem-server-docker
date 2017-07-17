82bf09f3678a21f6aab3be3164b9e88070fb954b
Pixel Perfect <pixelperfectpon3@gmail.com>
2017-07-16 20:47:43 -0400

    Revert most of the supression

--- install-fivem.sh
+++ install-fivem.sh
@@ -1,5 +1,5 @@
 wget http://updater.fivereborn.com/client/cfx-server.7z
-apt-get -y install p7zip-full 1>/dev/null
+apt-get -y install p7zip-full
 7z x cfx-server.7z
 cd cfx-server
 chmod u+x run.sh
--- install-mono.sh
+++ install-mono.sh
@@ -1,4 +1,4 @@
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF 1>/dev/null
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | tee /etc/apt/sources.list.d/mono-official.list
-apt-get update 1>/dev/null
-apt-get install mono-devel mono-complete -y 1>/dev/null
+apt-get update
+apt-get install mono-devel mono-complete -y
