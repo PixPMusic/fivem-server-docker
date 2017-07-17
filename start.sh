5c98892ea39de413a98e49e0e84e067fa869a91c
Pixel Perfect <pixelperfectpon3@gmail.com>
2017-07-17 00:30:50 -0400

    Probably still won't fix jack shit

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+if [ ! -f /opt/config/citmp-server.yml ]; then
+	cp /opt/cfx-server/citmp-server.yml /opt/config
+fi
 rm /opt/cfx-server/citmp-server.yml
 cp /opt/config/citmp-server.yml /opt/cfx-server/citmp-server.yml
+cd /opt/cfx-server
 sh /opt/cfx-server/run.sh
